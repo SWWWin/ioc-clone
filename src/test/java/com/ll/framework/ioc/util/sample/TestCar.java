cb56e1da69b1c7a5fc70c013bbe9ec09e85f40a1
SWWWin <bgpd1258@naver.com>
2025-08-20 15:40:55 +0900

    feat: t1 - green case

---
+++ src/test/java/com/ll/framework/ioc/util/sample/TestCar.java
@@ -0,0 +1,10 @@
+package com.ll.framework.ioc.util.sample;
+
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class TestCar {
+    private String name;
+    private int number;
+}
